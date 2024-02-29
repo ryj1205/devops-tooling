6965f112607975064678951c414837abff5e1ec4
ryj1205 <98744558+ryj1205@users.noreply.github.com>
2024-02-29 14:15:45 +0000

    Updated move-file.sh to move into right directory!

--- move-file.sh
+++ move-file.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mv ~/jenkins-pipelines/file.txt ~/jenkins-pipelines/file_$(date +"%d-%m-%Y").txt 
+mv ~/jenkins-pipelines/file.txt ~/jenkins-pipelines/processed/file_$(date +"%d-%m-%Y").txt 
